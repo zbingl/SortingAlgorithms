f02360cbecbeb6f1abb8e0747461f38e827b3896
zbingl <zbingl1234@gmail.com>
2025-03-28 11:02:42 +0100

    compiler

---
+++ compile.sh
@@ -0,0 +1,5 @@
+gcc src/MergeSort.c -o MergeSort
+gcc src/QuickSort.c -o QuickSort
+gcc src/BubbleSort.c -o BubbleSort
+gcc src/SelectionSort.c -o SelectionSort
+gcc src/InsertionSort.c -o InsertionSort
