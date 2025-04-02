6b183f67182e1f30031ea36cd0ac79b9d6c828cb
zbingl <zbingl1234@gmail.com>
2025-04-02 21:44:28 +0200

    GCC optimization

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
-gcc src/MergeSort.c -o MergeSort
-gcc src/QuickSort.c -o QuickSort
-gcc src/BubbleSort.c -o BubbleSort
-gcc src/SelectionSort.c -o SelectionSort
-gcc src/InsertionSort.c -o InsertionSort
+gcc -O3  src/MergeSort.c -o MergeSort
+gcc -O3 src/QuickSort.c -o QuickSort
+gcc -O3 src/BubbleSort.c -o BubbleSort
+gcc -O3 src/SelectionSort.c -o SelectionSort
+gcc -O3 src/InsertionSort.c -o InsertionSort
